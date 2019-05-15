288caf8969eb29f36e29d3997dc75bd706ab711f
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-15 15:51:35 -0600

    create new item working

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,44 @@
+class ItemsController < ApplicationController
+  # before_action :set_department #SETS DEPARTMENT FOR ALL CRUD ACTIONS
+
+
+  def index
+    @department = Department.find(params[:id])
+  end
+
+  def show
+    @department = Department.find(params[:id])
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @department = Department.find(params[:department_id])
+    @item = @department.items.new
+    # @topic = @sub.topics.new
+    render partial: 'form'
+  end
+
+  def create
+    @department = Department.find(params[:department_id])
+    @item = @department.items.new(params.require(:item).permit(:name, :description, :price))
+    if @item.save
+      redirect_to department_item_path(@department, @item)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+    def set_department
+      @department = Department.find(params[:id])
+    end
+
+
+end
+
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,5 @@
 class Department < ApplicationRecord
+
+has_many :items
+
 end
