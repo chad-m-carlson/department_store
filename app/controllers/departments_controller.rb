757f87f2ce24ab424c773da832cce45cbd23de81
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-15 14:57:41 -0600

    department controller crud actions complete

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,5 +8,36 @@ class DepartmentsController < ApplicationController
   end
 
   def new
+    @department = Department.new
+    render partial: "form"
   end
+
+  def create
+    @department = Department.new(params.require(:department).permit(:name, :description))
+    if @department.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+    render partial: "form"
+  end
+
+  def update
+    @department = Department.find(params[:id])
+    if @department.update(params.require(:department).permit(:name, :description))
+      redirect_to department_path
+    else
+      render partial: "form"
+    end
+  end
+
+  def destroy
+    @department = Department.find(params[:id]).destroy
+    redirect_to departments_path
+  end
+
 end
